d0f8a9bcc5f27421570455497fa503ea249d1c8b
JessFonseca <jfwolverine93@gmail.com>
2018-10-03 22:07:54 -0700

    Updates

--- js/main.js
+++ js/main.js
@@ -1,34 +1,30 @@
 //Select DOM Elements
 
-const btn = document.querySelector('.btn');
-const menu = document.querySelector('.menu');
-const menuNav = document.querySelector('.menu-nav');
-const menuContent = document.querySelector('.menu-content');
-const navItems = document.querySelectorAll('.nav-item');
+const btn = document.querySelector(".btn");
+const menu = document.querySelector(".menu");
+const menuNav = document.querySelector(".menu-nav");
+const menuContent = document.querySelector(".menu-content");
+const navItems = document.querySelectorAll(".nav-item");
 
 //Set state of the menu
 let showMenu = false;
 
-btn.addEventListener('click', toggleMenu);
+btn.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
-    if(!showMenu) {
-        btn.classList.add('close');
-        menu.classList.add('show');
-        menuNav.classList.add('show');
-        menuContent.classList.add('show');
-        navItems.forEach(item => item.classList.add('show'));
-        showMenu = true;
-
-    } else {
-        btn.classList.remove('close');
-        menu.classList.remove('show');
-        menuNav.classList.remove('show');
-        menuContent.classList.remove('show');
-        navItems.forEach(item => item.classList.remove('show'));
-        showMenu = false;
-
-    }
-
-
+  if (!showMenu) {
+    btn.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    menuContent.classList.add("show");
+    navItems.forEach(item => item.classList.add("show"));
+    showMenu = true;
+  } else {
+    btn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    menuContent.classList.remove("show");
+    navItems.forEach(item => item.classList.remove("show"));
+    showMenu = false;
+  }
 }
